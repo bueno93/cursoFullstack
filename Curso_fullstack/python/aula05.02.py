4e633b343b50c8c3344e5ee908d2390804af011f
Eduardo Bueno <128435849+bueno93@users.noreply.github.com>
2025-05-29 21:56:13 -0300

    Add files via upload

---
+++ Curso_fullstack/python/aula05.02.py
@@ -0,0 +1,28 @@
+while True:
+    print(" ------------------")
+    print( " |   MEU SISTEMA |")
+    print(" |  1 - Cliente   | ")
+    print(" |  2 - Produto   | ")
+    print(" |  3 - Vendas    | ")
+    print(" |  4 - Sair      |")
+    print(" ------------------")
+    opcao = int(input(" |  Digite a opção desejada : "))
+
+    if  opcao == 1 : 
+        print("Cliente")
+        continue
+    elif opcao == 2:
+        print("Produtos")
+        continue
+    elif opcao == 3:
+        print("Vendas")
+        continue
+    elif opcao == 4 :
+        print("Sair") 
+        break
+    else:
+        print("Digite uma opção valida")
+        continue
+        
+
+  
---
+++ Curso_fullstack/python/aula05.04.py
@@ -0,0 +1,17 @@
+alunos = []
+#funcao para adicionar um novo aluno ao array
+
+qtd_alunos = int(input("Digite a quantidade de alunos "))
+for i in range (0, qtd_alunos):
+   
+   nome = input("Nome:")
+   idade = input("Idade:")
+   nota = input("Nota")
+
+   aluno = {"nome":nome, "idade":idade,"nota":nota }
+   alunos.append(aluno)
+   
+print("Qtde de alunos ", len(alunos))
+
+for aluno in alunos :
+   print(aluno)
